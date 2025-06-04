3f07c6c6492ed6f2a0472a9f57cff6fb49ddd973
Oktomanus <dimithot@gmail.com>
2025-06-04 16:24:52 +0300

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+    set -Ux EDITOR helix
+    set -Ux VISUAL helix
+
+    fastfetch
+
+end
