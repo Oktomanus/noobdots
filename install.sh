03b4fa6263f57a910bf591529ae076e5e10e70e1
Oktomanus <dimithot@gmail.com>
2026-02-16 23:38:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -94,7 +94,8 @@ run_cmd "Installing main application list" sudo pacman -S --needed --noconfirm \
   firefox telegram-desktop fragments zed shotcut inkscape krita gimp gmic gimp-plugin-gmic upscayl imv audacity libreoffice obs-studio
 #   nvidia nvidia-settings libva-nvidia-driver cuda\
 # --- 5.1 Installing AUR packages using paru. Pluguins ---
-run_cmd "Installing anytype and pipes.sh via paru" paru -S --needed anytype pipes.sh catppuccin-gtk-theme-mocha catppuccin-gtk-theme-latte
+
+# run_cmd "Installing anytype and pipes.sh via paru" paru -S --needed anytype pipes.sh catppuccin-gtk-theme-mocha catppuccin-gtk-theme-latte
 
 run_cmd "Installing Yazi mount plugin" sh -c "yes | ya pkg add yazi-rs/plugins:mount"
 run_cmd "Installing Yazi Chmod plugin" sh -c "yes | ya pkg add yazi-rs/plugins:chmod"
