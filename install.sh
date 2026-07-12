37eeb9956029ebe3f9ecd0a75a37042b908a00a4
Oktomanus <dimithot@gmail.com>
2026-07-12 10:21:09 +0000

    Add nmap to terminal applications list

--- install.sh
+++ install.sh
@@ -74,7 +74,7 @@ install_packages() {
   fi
 
   CORE="hyprland xdg-desktop-portal-hyprland hyprlock hypridle hyprpicker hyprshot waybar mako ly base-devel udisks2"
-  TERMINAL="yazi foot fastfetch fish tmux btop bat ripgrep fd brightnessctl git openssh helix duf fzf eza zoxide calcurse 7zip libqalculate cava lolcat bluetui impala gping rustnet trippy s-tui speedtest-cli"
+  TERMINAL="yazi foot fastfetch fish tmux btop bat ripgrep fd brightnessctl git openssh helix duf fzf eza zoxide calcurse 7zip libqalculate cava lolcat bluetui impala gping rustnet trippy nmap s-tui speedtest-cli"
   FUN="cmatrix cowsay figlet toilet sl asciiquarium nyancat"
   MEDIA="imagemagick awww mpd mpc mpv easyeffects nwg-look wiremix rmpc"
   FONTS="ttf-jetbrains-mono-nerd ttf-firacode-nerd ttf-dejavu-nerd"
